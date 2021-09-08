11cae4fc46d986eb61f8ba1c0b664c1386d478cf
Paul York <paul@yorkfamily.com>
2021-09-08 15:06:01 -0400

    updated batch scripts

--- dbenv/env.cmd
+++ dbenv/env.cmd
@@ -1,10 +1,19 @@
 @echo off
 if defined DB_ENV_PATH (
-    set "PATH=%DB_ENV_PATH%"
-    set DB_ENV_PATH=
-    echo DBENV environment toggled OFF
+    if not "%~1"=="on" (
+        set "PATH=%DB_ENV_PATH%"
+        set DB_ENV_PATH=
+        echo DBENV environment OFF
+    ) else (
+        echo DBENV envronment ON
+    )
 ) else (
-    set "DB_ENV_PATH=%PATH%"
-    set "PATH=%CD%\python;%CD%\vscode\bin;%CD%\mariadb\bin;%PATH%"
-    echo DBENV environment toggled ON
+    if not "%~1"=="off" (
+        set "DB_ENV_PATH=%PATH%"
+        set "PATH=%~dp0python;%~dp0vscode\bin;%~dp0mariadb\bin;%PATH%"
+        popd
+        echo DBENV environment ON
+    ) else (
+        echo DBENV envronment OFF
+    )
 )
